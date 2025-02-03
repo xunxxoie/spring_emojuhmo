528f805d9b629e67dccaf7c4c9028aac9e9c27ca
xunxxoie <ahh0520@naver.com>
2025-02-03 22:55:40 +0900

    [Feat] Initial Commit

---
+++ src/main/java/joonseo/joonseo/JoonseoApplication.java
@@ -0,0 +1,13 @@
+package joonseo.joonseo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoonseoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoonseoApplication.class, args);
+    }
+
+}
